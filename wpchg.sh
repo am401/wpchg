dc59e5f8f968da5bc8cdc76c56e5fd127a1d9bf7
am401 <54643953+am401@users.noreply.github.com>
2019-09-11 14:01:46 -0500

    fixed file download location
    * Fixed a typo that prevented cron working properly
    * Changed initial wget to write to the file to the home file, in case script is not run within user's home folder

--- wpchg.sh
+++ wpchg.sh
@@ -9,21 +9,20 @@
 #------- description ------------------#
 #--------------------------------------#
 
-#  Script to change background wallpaper at 2AM everyday on
-#+ the device it is running on using cron jobs. This script
-#+ will run and initiate creating the cron job, the script
+#  Script to change background wallpaper on a Linux system.
+#+ The script will run and initiate creating the cron job
 #+ to change the wallpaper along with a monitoring script
 #+ that will ensure everything is on the system for the
-#+ change to take affect at 2AM.
+#+ change to take affect. It is currently set to execute at 2AM.
+#+ This was designed to make these changes without the need for
+#+ root/sudo access!
 
 site=       #  Set the website where the image will be obtained from
-file=       #  Define filename if need be - an extra line will need to be added
-            #+ if trying to rename to a specific file
+file=       #  Provide the filename to be downloaded and used
 
-wget $site  #  If using tinyurl, wget will keep the alias as the filename
-mv $file /home/$USER/$file
+wget -O $HOME/$file $site/$file  #  d/l background img to user's home dir
 
-# Set the background after running - currently this only works in Gnome
+#  Set the background after running - currently this only works in Gnome  
 gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/$file
 
 #  Create file .uwpc.bak which will be called by crontab to set the background
@@ -33,7 +32,8 @@ cat > /tmp/.uwpc.bak <<Write_UWPC
 #  Set necessary enviromental variables in order for cron to call the script
 #+ and gsettings to work properly
 PID=\$(pgrep gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=\$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/\$PID/environ|cut -d= -f2-)
+export DBUS_SESSION_BUS_ADDRESS=\$(grep -z DBUS_SESSION_BUS_ADDRESS \
+/proc/\$PID/environ|cut -d= -f2-)
 
 file=       #  Make sure that you set the same filename
 gsettings set org.gnome.desktop.background picture-uri file:///home/\$USER/\$file
@@ -49,13 +49,13 @@ cat > /tmp/.mps.daemon <<Write_MPS_Daemon
 while true; do
   #  Variables
   cron=\$(crontab -l)
-  chkCron="0 2 * * * ./tmp/.uwpc.bak"
+  chkCron="0 2 * * * /tmp/.uwpc.bak"  # Change this val if different time is needed
   site=                 #  Make sure you set the website to obtain image from
   file=                 #  Set the filename for the background image
 
   # Check if cronjob still exists for user
   if [ "\$cron" != "\$chkCron" ]; then
-    echo "0 2 * * * ./tmp/.uwpc.bak" > /tmp/cron
+    echo "0 2 * * * /tmp/.uwpc.bak" > /tmp/cron # Make sure this matches \$chkCron
     crontab /tmp/cron   # Push the above line to crontab
     rm /tmp/cron        # Delete the file once done
   fi
@@ -73,12 +73,13 @@ while true; do
 #!/bin/bash
 file=       #  Make sure that you set the same filename
 PID=$(pgrep gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS \
+/proc/$PID/environ|cut -d= -f2-)
 gsettings set org.gnome.desktop.background picture-uri file:///home/\$USER/\$file 
 Write_UWPC_from_Daemon
 chmod +x /tmp/.uwpc.bak
   fi
-     sleep 3600 # Run these checks every hour
+     sleep 1500 # Run these checks every 25 minutes
 done
 Write_MPS_Daemon
 chmod +x /tmp/.mps.daemon
